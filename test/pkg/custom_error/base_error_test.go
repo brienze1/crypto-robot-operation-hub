bb8a3ebae487ed0821ea273ea323afa633f64d46
Luis Brienze <lfbrienze@gmail.com>
2022-08-21 20:01:55 -0300

    [TEST-CI/CD] - added more test coverage, removed cache from lint
    Signed-off-by: Luis Brienze <lfbrienze@gmail.com>

---
+++ test/pkg/custom_error/base_error_test.go
@@ -0,0 +1,61 @@
+package custom_error
+
+import (
+	"errors"
+	"github.com/brienze1/crypto-robot-operation-hub/pkg/custom_error"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type baseErrorExample struct {
+	Message            string
+	InternalMessage    string
+	DescriptionMessage string
+}
+
+func (b baseErrorExample) Error() string {
+	return b.Message
+}
+
+func (b baseErrorExample) InternalError() string {
+	return b.InternalMessage
+}
+
+func (b baseErrorExample) Description() string {
+	return b.DescriptionMessage
+}
+
+var (
+	errorTest     error
+	baseErrorTest baseErrorExample
+)
+
+func setup() {
+	errorTest = errors.New("error Message")
+
+	baseErrorTest = baseErrorExample{
+		Message:            "error Message",
+		InternalMessage:    "error InternalMessage",
+		DescriptionMessage: "error DescriptionMessage",
+	}
+}
+
+func TestBaseExceptionFromErrorSuccess(t *testing.T) {
+	setup()
+
+	baseError := custom_error.BaseError(errorTest)
+
+	assert.Equal(t, errorTest.Error(), baseError.Message)
+	assert.Equal(t, "", baseError.InternalMessage)
+	assert.Equal(t, "", baseError.DescriptionMessage)
+}
+
+func TestBaseExceptionFromBaseErrorSuccess(t *testing.T) {
+	setup()
+
+	baseError := custom_error.BaseError(baseErrorTest)
+
+	assert.Equal(t, baseErrorTest.Message, baseError.Message)
+	assert.Equal(t, baseErrorTest.InternalMessage, baseError.InternalMessage)
+	assert.Equal(t, baseErrorTest.DescriptionMessage, baseError.DescriptionMessage)
+}
