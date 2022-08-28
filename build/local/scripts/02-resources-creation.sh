23d8816f5f458e4b802ea9cfae35d78791ba49b1
Luis Brienze <lfbrienze@gmail.com>
2022-08-28 17:42:25 -0300

    [BUILD] - created dynamodb crypto_robot.clients table in localstack scripts
    Signed-off-by: Luis Brienze <lfbrienze@gmail.com>

--- build/local/scripts/02-resources-creation.sh
+++ build/local/scripts/02-resources-creation.sh
@@ -27,10 +27,25 @@ aws sns subscribe \
 echo "########### Listing SNS Subscriptions ###########"
 aws sns list-subscriptions --endpoint-url http://localhost:4566
 
-echo "########### Creating DynamoDB 'crypto_robot.analyzed_data' table ###########"
+echo "########### Creating DynamoDB 'crypto_robot.clients' table ###########"
 aws dynamodb create-table \
 --table-name crypto_robot.clients  \
 --attribute-definitions AttributeName=client_id,AttributeType=S \
---key-schema AttributeName=client_id,KeyType=HASH \
+AttributeName=locked_until,AttributeType=S \
+AttributeName=active,AttributeType=B \
+AttributeName=locked,AttributeType=B \
+AttributeName=cash_amount,AttributeType=N \
+AttributeName=crypto_amount,AttributeType=N \
+AttributeName=buy_on,AttributeType=N \
+AttributeName=sell_on,AttributeType=N \
+--key-schema AttributeName=client_id,KeyType=HASH AttributeName=locked_until,KeyType=RANGE \
+--global-secondary-indexes "[ \
+{\"IndexName\":\"active-index\",\"KeySchema\":[{\"AttributeName\":\"active\",\"KeyType\":\"HASH\"}],\"Projection\":{\"ProjectionType\":\"ALL\"},\"ProvisionedThroughput\":{\"ReadCapacityUnits\":5,\"WriteCapacityUnits\":5}}, \
+{\"IndexName\":\"locked-index\",\"KeySchema\":[{\"AttributeName\":\"locked\",\"KeyType\":\"HASH\"}],\"Projection\":{\"ProjectionType\":\"ALL\"},\"ProvisionedThroughput\":{\"ReadCapacityUnits\":5,\"WriteCapacityUnits\":5}}, \
+{\"IndexName\":\"cash_amount-index\",\"KeySchema\":[{\"AttributeName\":\"cash_amount\",\"KeyType\":\"HASH\"}],\"Projection\":{\"ProjectionType\":\"ALL\"},\"ProvisionedThroughput\":{\"ReadCapacityUnits\":5,\"WriteCapacityUnits\":5}}, \
+{\"IndexName\":\"crypto_amount-index\",\"KeySchema\":[{\"AttributeName\":\"crypto_amount\",\"KeyType\":\"HASH\"}],\"Projection\":{\"ProjectionType\":\"ALL\"},\"ProvisionedThroughput\":{\"ReadCapacityUnits\":5,\"WriteCapacityUnits\":5}}, \
+{\"IndexName\":\"buy_on-index\",\"KeySchema\":[{\"AttributeName\":\"buy_on\",\"KeyType\":\"HASH\"}],\"Projection\":{\"ProjectionType\":\"ALL\"},\"ProvisionedThroughput\":{\"ReadCapacityUnits\":5,\"WriteCapacityUnits\":5}}, \
+{\"IndexName\":\"sell_on-index\",\"KeySchema\":[{\"AttributeName\":\"sell_on\",\"KeyType\":\"HASH\"}],\"Projection\":{\"ProjectionType\":\"ALL\"},\"ProvisionedThroughput\":{\"ReadCapacityUnits\":5,\"WriteCapacityUnits\":5}} \
+]" \
 --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 \
 --endpoint-url=http://localhost:4566
